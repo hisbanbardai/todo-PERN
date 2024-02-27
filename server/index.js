2d65c7b8e6e250ffd32857f0a6778393a4201c1f
hisbanshiraz <hisbanbardai@gmail.com>
2024-02-26 20:44:44 -0700

    Add create a todo route

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const pool = require("./db/connection");
-require("dotenv").config();
 
 const PORT = process.env.PORT;
 
@@ -11,6 +11,27 @@ const app = express();
 //Middleware
 app.use(cors());
 app.use(express.json());
+
+//ROUTES
+
+//get all todos
+
+//get a todo
+
+//create a todo
+app.post("/todos", (req, res) => {
+  const { description } = req.body;
+  pool
+    .query(`INSERT into todo (description) VALUES ($1) RETURNING *`, [
+      description,
+    ])
+    .then((data) => res.json({ todo: data.rows[0] }))
+    .catch((error) => console.error(error.message));
+});
+
+//update a todo
+
+//delete a todo
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
