c0a09768499fd48ebdc56eec83dd9193ac7c6e26
hisbanshiraz <hisbanbardai@gmail.com>
2024-02-28 21:21:58 -0700

    Add form and h1 elements in todo input component

--- client/src/components/TodoInput/TodoInput.jsx
+++ client/src/components/TodoInput/TodoInput.jsx
@@ -1,3 +1,11 @@
 export default function TodoInput() {
-  return <h1>Input</h1>;
+  return (
+    <>
+      <h1 className="main-heading">Input Todo</h1>
+      <form className="input-todo-form">
+        <input type="text" placeholder="add todo" />
+        <button className="btn-add">Add</button>
+      </form>
+    </>
+  );
 }
