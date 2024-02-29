e4916c3727947ba0fccb71a8f8349b22a46a5945
hisbanshiraz <hisbanbardai@gmail.com>
2024-02-28 22:19:01 -0700

    Add handleFormSubmit function and implement a post fetch request to create a todo item

--- client/src/components/TodoInput/TodoInput.jsx
+++ client/src/components/TodoInput/TodoInput.jsx
@@ -4,10 +4,32 @@ import styles from "./TodoInput.module.css";
 export default function TodoInput() {
   const [todoInput, setTodoInput] = useState("");
 
+  async function handleFormSubmit(e) {
+    e.preventDefault();
+    setTodoInput("");
+
+    const body = { description: todoInput };
+
+    try {
+      const response = await fetch(`http://localhost:8081/todos`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      });
+
+      if (!response.ok) {
+        throw new Error(`Unable to add todo item`);
+      }
+      console.log(await response.json());
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
   return (
     <>
       <h1 className={styles.mainHeading}>Input Todo</h1>
-      <form className={styles.inputTodoForm}>
+      <form className={styles.inputTodoForm} onSubmit={handleFormSubmit}>
         <input
           className={styles.inputTodo}
           type="text"
