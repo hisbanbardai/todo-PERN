18d6fc17f207fdc6e2f9d14a629ac6d80c88595b
hisbanshiraz <hisbanbardai@gmail.com>
2024-02-28 21:45:55 -0700

    Add state for todo input

--- client/src/components/TodoInput/TodoInput.jsx
+++ client/src/components/TodoInput/TodoInput.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import styles from "./TodoInput.module.css";
 
 export default function TodoInput() {
+  const [todoInput, setTodoInput] = useState("");
+
   return (
     <>
       <h1 className={styles.mainHeading}>Input Todo</h1>
@@ -9,6 +12,8 @@ export default function TodoInput() {
           className={styles.inputTodo}
           type="text"
           placeholder="add todo"
+          value={todoInput}
+          onChange={(e) => setTodoInput(e.target.value)}
         />
         <button className={styles.btnAdd}>Add</button>
       </form>
