de9730c0e9f84407ff67b8fa7909e5ad81f9bd7b
hisbanshiraz <hisbanbardai@gmail.com>
2024-02-29 20:30:40 -0700

    Add useEffect to get all todos

--- client/src/components/TodoList/TodoList.jsx
+++ client/src/components/TodoList/TodoList.jsx
@@ -1,6 +1,20 @@
+import { useEffect } from "react";
 import styles from "./TodoList.module.css";
 
 export default function TodoList() {
+  async function getTodos() {
+    try {
+      const response = await fetch(`http://localhost:8081/todos`);
+      console.log(await response.json());
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
   return (
     <>
       <h1 className={styles.mainHeading}>List of Todos</h1>
